a2fa1afe1b926269c284fe1149719f0061383bf7
sonyvarade <121173056+sonyvarade@users.noreply.github.com>
2023-08-05 12:54:11 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws s3 cp s3://codebuildjartifacts/Code_build_tomact_cicd_new/java-tomcat-maven-example.war /tmp
 sudo mv /tmp/java-tomcat-maven-example.war /opt/apache-tomcat-9.0.78/webapps/
-sudo service tomcat restart
+sudo tomcatup3
